2232c3153cbcaf8518e57e015312978ac4d1c52c
migitera.yusuke <migitera.yusuke@donuts.ne.jp>
2022-03-17 16:26:50 +0900

    update: MainActivity

--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.weather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.viewModels
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewModel.loadForecast()
+
+        val textView = findViewById<TextView>(R.id.textView)
+        viewModel.forecast.observe(this) {
+            textView.setText(viewModel.forecast.value?.cod)
+        }
     }
 }
